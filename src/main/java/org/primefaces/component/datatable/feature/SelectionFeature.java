15e19a1e55d08b24f5b966e2623e0fa536904b94
mertsincan <sincan.mert@gmail.com>
2017-02-17 17:55:37 +0300

    Fixed ref2096

--- src/main/java/org/primefaces/component/datatable/feature/SelectionFeature.java
+++ src/main/java/org/primefaces/component/datatable/feature/SelectionFeature.java
@@ -37,7 +37,6 @@ public class SelectionFeature implements DataTableFeature {
         String clientId = table.getClientId(context);
 		Map<String,String> params = context.getExternalContext().getRequestParameterMap();
 		String selection = params.get(clientId + "_selection");
-        Object originalValue = table.getValue();
         Object filteredValue = table.getFilteredValue();
         boolean isFiltered = (filteredValue != null);
         
@@ -50,6 +49,7 @@ public class SelectionFeature implements DataTableFeature {
 		else
 			decodeMultipleSelection(context, table, selection);
         
+        Object originalValue = table.getValue();
         if(isFiltered) {
             table.setValue(originalValue);
         }
